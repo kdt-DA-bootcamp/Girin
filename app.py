28b842c67f9086a5152f7188e03c0916a0e67ef4
Girin-man <kimjh3940@gmail.com>
2025-01-17 12:00:07 +0900

    Second commit

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
 st.title('Girin\'s Deploy App')
+st.write('My Second Deploy App!')
 
-st.write('My First Deploy App!')
+import os
+
+key = os.environ.get('MY_SECRET', 'NOT SET YET')
+st.write(f'Server Key : {key}')
+
+
